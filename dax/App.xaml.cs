9fce780c69b90e771a9936e5e254a381129fce3a
Ruslan Absalyamov <ruslan.absalyamov@gmail.com>
2015-07-21 21:19:16 +0300

    The same culture for ui. #27

--- dax/App.xaml.cs
+++ dax/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
 
 namespace dax
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture;
+		}
+	}
 }
